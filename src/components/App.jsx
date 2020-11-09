f0c4b25024e572de1f7ac8b1aa4905a5efd43253
somoye123 <somoye.ayotunde@gmail.com>
2020-11-09 15:53:41 +0100

    add homePage and error page route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Route, Redirect, Switch } from "react-router-dom";
-import HomePage from '..'
-
+import { Route, Switch, useHistory } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import PageNotFound from '../pages/ErrorPage';
+import Auth from '../Auth0/auth';
 
 function App() {
-  return <h1>somoye</h1>;
+  const history = useHistory();
+  const auth = new Auth(history);
+
+  return (
+    <>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => <HomePage auth={auth} />}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
