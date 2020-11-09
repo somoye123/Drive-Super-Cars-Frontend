d6d7197f7ac99e8b7dce50ebdc5687e9f0c93a7c
somoye123 <somoye.ayotunde@gmail.com>
2020-11-09 15:53:58 +0100

    build application home page bare bone structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const HomePage = ({ auth }) => {
   const { login } = auth;
