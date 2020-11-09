bf42089326c83236d7a6de61061b59e66a37236d
somoye123 <somoye.ayotunde@gmail.com>
2020-11-09 11:19:06 +0100

    wrap application inside of browserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
