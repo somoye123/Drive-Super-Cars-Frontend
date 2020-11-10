2f40d263819f0c1880b75eff2f31075697b31162
somoye123 <somoye.ayotunde@gmail.com>
2020-11-10 11:02:51 +0100

    update auth module with authentication logic

--- src/Auth0/auth.js
+++ src/Auth0/auth.js
@@ -1,6 +1,7 @@
 import auth0 from 'auth0-js';
 
-export default function Auth() {
+export default function Auth(historyValue) {
+  const history = historyValue;
   const auth = new auth0.WebAuth({
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
     clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -11,7 +12,56 @@ export default function Auth() {
   });
 
   const login = () => auth.authorize();
+
+  const setSession = authResult => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime(),
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  };
+
+  const handleAuthentication = () => {
+    auth.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        setSession(authResult);
+        history.push('/car-list');
+      } else if (err) {
+        history.push('/error');
+        throw new Error(err);
+      }
+    });
+  };
+
+  const isAuthenticated = () => {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  };
+
+  const logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    auth.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      returnTo: process.env.REACT_APP_API_URL,
+    });
+  };
+
+  const getAccessToken = () => {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('No access token found.');
+    }
+    return accessToken;
+  };
+
   return {
     login,
+    isAuthenticated,
+    logout,
+    handleAuthentication,
+    getAccessToken,
   };
 }
