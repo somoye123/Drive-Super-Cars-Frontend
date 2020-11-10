4c95cb803c9c3caeb1f9fdf0608e108d7adc70cb
somoye123 <somoye.ayotunde@gmail.com>
2020-11-10 11:39:50 +0100

    add logic to userEffect

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const HomePage = () => (
-  <>
-    <h1>Home</h1>
-  </>
-);
+const HomePage = ({ auth }) => {
+  const location = useLocation();
+  React.useEffect(() => {
+    if (/access_token|id_token|error/.test(location.hash)) {
+      auth.handleAuthentication();
+    }
+  }, [location]);
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
 
+HomePage.propTypes = {
+  auth: PropTypes.func.isRequired,
+};
 export default HomePage;
