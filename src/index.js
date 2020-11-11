dda5259085445918efe41785313e46cf3bf2a5da
somoye123 <somoye.ayotunde@gmail.com>
2020-11-11 08:40:36 +0100

    wrap entire application inside redux provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import { Provider as ReduxProvider } from 'react-redux';
 import App from './components/App';
+import ConfigureStore from './redux/configureStore';
+
+const store = ConfigureStore();
 
 render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ReduxProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
