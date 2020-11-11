1fca4a18ccc1042d630800eebcc15943d71a7483
somoye123 <somoye.ayotunde@gmail.com>
2020-11-11 08:37:35 +0100

    build application action types for redux

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const SET_CARS = 'SET_CARS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_CAR = 'SET_CAR';
+export const SET_USER = 'SET_USER';
+export const SET_APPOINTMENT = 'SET_APPOINTMENT';
