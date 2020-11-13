5d65885d8f2cbaefc9b37ac07694b76d5a8b67b5
somoye123 <somoye.ayotunde@gmail.com>
2020-11-14 00:34:17 +0100

    add appointment reducer to application root reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import User from './userReducer';
 import Cars from './carsReducer';
 import Car from './carReducer';
+import Appointment from './appointmentReducer';
 
 export default combineReducers({
-  User, Cars, Car,
+  User, Cars, Car, Appointment,
 });
