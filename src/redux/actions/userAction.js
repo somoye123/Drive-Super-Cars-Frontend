03cda70bfd905d4af110d04c3164b3c67acbb413
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 23:00:41 +0100

    update userApi import to a named export

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,13 +1,13 @@
 import { toast } from 'react-toastify';
 import * as types from './actionTypes';
-import UserApi from '../../api/userApi';
+import { getUser } from '../../api/userApi';
 
 export const loadUserSuccess = user => ({ type: types.SET_USER, user });
 
 export function loadUser(body, token) {
   return async dispatch => {
     try {
-      const data = await UserApi(body, token);
+      const data = await getUser(body, token);
       if (data) { dispatch(loadUserSuccess(data)); }
     } catch (error) {
       toast.error(`Whoops!, ${error.message} occured`);
