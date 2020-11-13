90d579e3339830818da5a253844579ade9d9e459
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 23:02:18 +0100

    implement creating appointment logic

--- src/pages/SingleCarPage.jsx
+++ src/pages/SingleCarPage.jsx
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useParams, Link } from 'react-router-dom';
 import { loadCar, loadCarSuccess } from '../redux/actions/carAction';
+import { createAppointment } from '../api/userApi';
 
 const SingleCar = ({
-  Car, loadCar, loadCarSuccess, auth,
+  Car, loadCar, loadCarSuccess, auth, User,
 }) => {
   const { id } = useParams();
   const loadSingleCarDetails = () => {
@@ -32,23 +33,26 @@ const SingleCar = ({
       <h2 className="section-title">{name}</h2>
       <img src={`${process.env.REACT_APP_SERVER_BLOB_PATH}${Car.img_url}`} alt="car" />
       <p>{description}</p>
+      <button type="button" onClick={() => createAppointment(User.id, Car.id, auth.getAccessToken())}>test drive</button>
     </section>
   );
 };
 
 SingleCar.defaultProps = {
-  Car: null,
   auth: null,
+  Car: null,
+  User: null,
 };
 
 SingleCar.propTypes = {
-  Car: PropTypes.object || null,
   auth: PropTypes.object || null,
+  Car: PropTypes.object || null,
+  User: PropTypes.object || null,
   loadCar: PropTypes.func.isRequired,
   loadCarSuccess: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({ Car }) => ({ Car });
+const mapStateToProps = ({ Car, User }) => ({ Car, User });
 
 const mapDispatchToProps = { loadCar, loadCarSuccess };
 
