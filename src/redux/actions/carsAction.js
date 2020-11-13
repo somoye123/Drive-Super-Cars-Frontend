66e57969660eab2f04ba2038a6cbf6d73fdf985c
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 09:58:20 +0100

    build cars action

---
+++ src/redux/actions/carsAction.js
@@ -0,0 +1,16 @@
+import { toast } from 'react-toastify';
+import * as types from './actionTypes';
+import { getCars } from '../../api/carApi';
+
+export const loadCarsSuccess = cars => ({ type: types.SET_CARS, cars });
+
+export function loadCars(token) {
+  return async dispatch => {
+    try {
+      const data = await getCars(token);
+      if (data) { dispatch(loadCarsSuccess(data)); }
+    } catch (error) {
+      toast.error(`Whoops!, ${error.message} occured`);
+    }
+  };
+}
