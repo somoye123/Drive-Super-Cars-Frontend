ad4f8cf7489a192c10c6335c8bf5e364d621767e
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 23:01:28 +0100

    add method for creating appointment

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,8 +1,10 @@
+import { toast } from 'react-toastify';
 import { handleResponse, handleError } from './apiUtils';
 
 const baseUrl = `${process.env.REACT_APP_SERVER_API_URL}/users`;
+const createAppointmentUrl = `${process.env.REACT_APP_SERVER_API_URL}/appointments`;
 
-export default (body, token) => fetch(baseUrl, {
+export const getUser = (body, token) => fetch(baseUrl, {
   method: 'POST',
   headers: {
     'content-type': 'application/json',
@@ -12,3 +14,21 @@ export default (body, token) => fetch(baseUrl, {
 })
   .then(handleResponse)
   .catch(handleError);
+
+// eslint-disable-next-line camelcase
+export const createAppointment = async (user_id, car_id, token) => {
+  try {
+    const response = await fetch(createAppointmentUrl, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ user_id, car_id }),
+    });
+    if (response.ok || response.status === 201) return toast.success('appointment created successfully');
+    throw new Error('Network response was not ok.');
+  } catch (error) {
+    return toast.error('Whoops!, it seems you have booked an appointment with this car');
+  }
+};
