69fb8b84c7d58b397ea9d40d6d82061439c59e88
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 03:08:19 +0100

    update handleResponse to allow 201 status code

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,5 +1,5 @@
 export async function handleResponse(response) {
-  if (response.ok) return response.json();
+  if (response.ok || response.status === 201) return response.json();
   if (response.status === 400) {
     const error = await response.text();
     throw new Error(error);
