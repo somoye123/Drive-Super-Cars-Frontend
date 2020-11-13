2ee637f1a001f3dd3d1074d6106d483181a40db1
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 13:48:21 +0100

    add car reducer to application root reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import User from './userReducer';
 import Cars from './carsReducer';
+import Car from './carReducer';
 
 export default combineReducers({
-  User, Cars,
+  User, Cars, Car,
 });
