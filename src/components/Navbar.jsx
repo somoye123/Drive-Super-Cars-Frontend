f159cb322624be198fc8fe15483e449e263d69d1
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 22:53:44 +0100

    update navbar brand color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = ({ auth }) => {
 
     <header className="px-md-5 navbar-light bg-white fixed-top">
       <nav className="navbar navbar-expand-lg px-md-5">
-        <h3 className="navbar-brand text-primary font-weight-bold">Drive Super Cars</h3>
+        <h3 className="navbar-brand text-danger font-weight-bold">Drive Super Cars</h3>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav ml-auto align-items-center">
             <li>
               <NavLink to="/" className="nav-link" activeStyle={activeStyle} exact>Home</NavLink>
             </li>
