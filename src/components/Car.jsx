0dc23cf8d0cf5423852ab5bf6ae86638277209af
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 20:01:31 +0100

    update image source for car component

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Car = ({ car }) => (
   <article className="car">
     <div className="img-container">
-      <img src={`${process.env.REACT_APP_SERVER_BLOB_PATH}${car.img_url}`} alt="car" />
+      <img src={car.img_url} alt="car" />
       <Link to={`/car/${car.id}`} className="banner-btn car-link">
         Features
       </Link>
