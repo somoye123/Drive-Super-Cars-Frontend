8ccf7bc7ccd60c92f36d1c586cfe6fc7f1abf0c1
somoye123 <somoye.ayotunde@gmail.com>
2020-11-16 10:56:18 +0100

    add loading reducer to application combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,8 @@ import User from './userReducer';
 import Cars from './carsReducer';
 import Car from './carReducer';
 import Appointments from './appointmentsReducer';
+import Loading from './loadingReducer';
 
 export default combineReducers({
-  User, Cars, Car, Appointments,
+  User, Cars, Car, Appointments, Loading,
 });
