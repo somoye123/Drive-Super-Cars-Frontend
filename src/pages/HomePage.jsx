46956e107e436ccf166329c2ba8158aadc86a3d9
somoye123 <somoye.ayotunde@gmail.com>
2020-11-16 10:59:12 +0100

    update homepage to utilize redux laoding state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import LoadingBar from '../components/Loading';
 import { loadUser } from '../redux/actions/userAction';
+import setLoadingStatus from '../redux/actions/loadingAction';
 
-const HomePage = ({ auth, loadUser, User }) => {
+const HomePage = ({
+  auth,
+  loadUser,
+  User,
+  setLoadingStatus,
+  Loading,
+}) => {
   const loadUserProfile = () => {
     let body;
     const token = auth.getAccessToken();
@@ -21,8 +29,11 @@ const HomePage = ({ auth, loadUser, User }) => {
   React.useEffect(() => {
     if (auth.isAuthenticated() && (Object.keys(User).length === 0 && User.constructor === Object)) {
       loadUserProfile();
+      setLoadingStatus(false);
     }
   }, []);
+
+  if (Loading) return <LoadingBar />;
 
   return (
     <main className="defaultHero d-flex align-items-center justify-content-center px-4 px-md-0">
@@ -47,10 +58,12 @@ HomePage.propTypes = {
   auth: PropTypes.object || null,
   User: PropTypes.object || null,
   loadUser: PropTypes.func.isRequired,
+  Loading: PropTypes.bool.isRequired,
+  setLoadingStatus: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({ User }) => ({ User });
+const mapStateToProps = ({ User, Loading }) => ({ User, Loading });
 
-const mapDispatchToProps = { loadUser };
+const mapDispatchToProps = { loadUser, setLoadingStatus };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
